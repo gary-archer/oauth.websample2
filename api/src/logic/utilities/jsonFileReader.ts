171db077e1b1ab2ce4baf754f11b3064229623b4
gary-archer <garyarcher36@gmail.com>
2025-04-10 19:05:00 +0100

    Updates

--- api/src/logic/utilities/jsonFileReader.ts
+++ api/src/logic/utilities/jsonFileReader.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra';
-import {ServerError} from '../errors/serverError.js';
 import {ErrorCodes} from '../errors/errorCodes.js';
+import {ServerError} from '../errors/serverError.js';
 
 /*
  * A simple utility to deal with the infrastructure of reading JSON files
@@ -8,20 +8,18 @@ import {ErrorCodes} from '../errors/errorCodes.js';
 export class JsonFileReader {
 
     /*
-     * Do the file reading and return a promise
+     * Read the data and handle errors
      */
     public async readData<T>(filePath: string): Promise<T> {
 
         try {
 
-            // Try the read
-            const buffer = await fs.readFile(filePath);
-            return JSON.parse(buffer.toString()) as T;
+            const json = await fs.readFile(filePath, 'utf8');
+            return JSON.parse(json) as T;
 
         } catch (e: any) {
 
-            // Do error translation of file read errors
-            throw new ServerError(ErrorCodes.fileReadError, 'Problem encountered accessing data', e.stack);
+            throw new ServerError(ErrorCodes.fileReadError, `Problem encountered reading file ${filePath}`, e.stack);
         }
     }
 }
